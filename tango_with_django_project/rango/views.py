e8cfa2258c3c8637a4ee14f5afae005c4cd5f123
Neill Henning <neillhenning@gmail.com>
2015-01-04 15:23:31 +0200

    Add models templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category
+from rango.models import Page
 
 # old index(request) function below, replaced by info provided in:
 # http://www.tangowithdjango.com/book17/chapters/models_templates.html
@@ -29,3 +30,32 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': "This is my own bold message from context"}
     return render(request, 'rango/about.html', context_dict)
+
+def category(request, category_name_slug):
+
+    # Create a context dictionary which we can pass to the template rendering engine.
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't the .get() method raises a DoesNotExist execption.
+        # So the .get() method returns one model instance or raises an exception.
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        # Retrieve all of the associated pages.
+        # Note that filter returns >= model instances.
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+        # We also add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything - the template displays the "no category" message or us.
+        pass
+
+    # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context_dict)
